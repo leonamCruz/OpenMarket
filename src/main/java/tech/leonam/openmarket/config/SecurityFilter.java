dee35eb307eee8486555761f8befe0e6e91102f0
Leonam <leonam447@gmail.com>
2023-10-17 11:45:28 -0300

    Space add

--- src/main/java/tech/leonam/openmarket/config/SecurityFilter.java
+++ src/main/java/tech/leonam/openmarket/config/SecurityFilter.java
@@ -29,7 +29,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
 
-        if(token!= null){
+        if(token != null){
             var login = tokenService.validationToken(token);
             UserDetails user = loginService.loadUserByUsername(login);
 
