42ef9466fe1da3a8778197d60aa7086c8ef3b508
Leonam <leonam447@gmail.com>
2023-10-17 20:25:16 -0300

    Remove imports unnecessary

--- src/main/java/tech/leonam/openmarket/model/entity/LoginEntity.java
+++ src/main/java/tech/leonam/openmarket/model/entity/LoginEntity.java
@@ -2,7 +2,6 @@ package tech.leonam.openmarket.model.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
