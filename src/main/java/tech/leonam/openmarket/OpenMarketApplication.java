4eb2fbd98b6b58ffd123728c19c7aa005e94d220
Leonam <leonam447@gmail.com>
2023-10-17 11:53:27 -0300

    add modelmapp

--- src/main/java/tech/leonam/openmarket/OpenMarketApplication.java
+++ src/main/java/tech/leonam/openmarket/OpenMarketApplication.java
@@ -1,7 +1,9 @@
 package tech.leonam.openmarket;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class OpenMarketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenMarketApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
