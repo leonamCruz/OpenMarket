86f4677296ba1b0802ffc9e0df75917872838b23
Leonam <leonam447@gmail.com>
2023-10-22 19:36:22 -0300

    Unit`s Add

--- src/main/java/tech/leonam/openmarket/model/enums/UnitMeasurementEnum.java
+++ src/main/java/tech/leonam/openmarket/model/enums/UnitMeasurementEnum.java
@@ -1,4 +1,25 @@
 package tech.leonam.openmarket.model.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum UnitMeasurementEnum {
+    UNIT(0,"Unidade(s)"),
+    DOZEN(1,"Dúzia"),
+    KG(2,"Quilo(s)");
+
+    private final int code;
+    private final String description;
+
+    public static UnitMeasurementEnum descriptionToEnum(String description) {
+        for (var unitMeasurementEnum : values()) {
+            if (unitMeasurementEnum.description.equals(description)) {
+                return unitMeasurementEnum;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
 }
